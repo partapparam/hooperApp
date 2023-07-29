0c2bcc2419be86c686548165b31ffc5c972f4141
partapparam <paramsingh1961@gmail.com>
2023-07-28 21:56:37 -0700

    apollo client setup with ws

---
+++ src/graphql/graphql.client.js
@@ -0,0 +1,47 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  split,
+} from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
+import { getMainDefinition } from "@apollo/client/utilities"
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions"
+import { createClient } from "graphql-ws"
+
+// the app must have an Http connection as well as WS Connection to our Graphql server
+const httpLink = createHttpLink({
+  uri: `${process.env.VITE_APP_API_URL}`,
+})
+const wsLink = new GraphQLWsLink(
+  createClient({ url: `${process.env.VITE_APP_API_WS_URL}` })
+)
+
+// Define Context Header object so that possible token from Firebase is set to the header auth for each request to server
+const authLink = setContext((_, { headers }) => {
+  // const token = localStorage.getItem('firebase')
+  const token = "token"
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  }
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: splitLink,
+})
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import { client } from "./graphql/graphql.client.js"
+import { ApolloProvider } from "@apollo/client"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>
 )
