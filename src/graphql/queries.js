bc6a4ffbe789327829451ae90602dcf4e8d787aa
partapparam <paramsingh1961@gmail.com>
2023-07-30 16:51:12 -0700

    query setup

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -13,3 +13,13 @@ export const PLAYER_DETAILS = gql`
     location
   }
 `
+
+export const GET_ALL_PLAYERS = gql`
+  query {
+    GetAllPlayers {
+      ...PlayerDetails
+    }
+  }
+  ${PLAYER_DETAILS}
+`
+export const GET_PLAYER_PROFILE_BY_NAME = gql``
