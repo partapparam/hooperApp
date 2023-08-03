85e947c0faecdd988c6426e5259ab24be89b311f
partapparam <paramsingh1961@gmail.com>
2023-08-02 21:58:32 -0700

    game card

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,9 +2,8 @@ import React from "react"
 
 export const GameCard = ({ game }) => {
   return (
-    <div className="flex flex-col p-3 my-3 shadow-lg bg-gray-300">
-      <h3>Players in the Game: {game.playerCount}</h3>
-      <div>
+    <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 sm:flex-row p-3 my-3 shadow-lg items-center	justify-center text-center bg-gray-300 ">
+      <div className="p-2">
         <h3>Home Team</h3>
         {game.homeTeam.map((player) => {
           return (
@@ -15,7 +14,10 @@ export const GameCard = ({ game }) => {
           )
         })}
       </div>
-      <div>
+      <div className="p-2">
+        <h3>vs.</h3>
+      </div>
+      <div className="p-2">
         <h3>Away Team</h3>
         {game.awayTeam.map((player) => {
           return (
